da21ed2faaba6b0c70b87d00d1cba9068f21693d
Niddam <louniddam@gmail.com>
2021-04-15 16:39:25 +0200

    styled-components library and CSS modules used

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,41 @@
-import React from 'react';
+// import styled from 'styled-components'
+import React from 'react'
+import styles from './Button.module.css'
 
-import './Button.css';
+
+//METHODE AVEC LIBRAIRIE STYLED-COMPONENTS
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+
+//   @media (min-width:768px) {
+//     width: auto;
+//   }
+// `;
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    //si on a une classe avec un - comme button-submit à la place de styles.button on fait styles.['button-submit']
+    //Si j'ai besoin de rajouter une classe conditionnelle comme invalid je fais
+    //className={`${styles.button} ${!isValid && styles.invalid}`}
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
